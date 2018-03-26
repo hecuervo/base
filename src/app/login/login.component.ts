c635e5a84852309c6e61061c5fc831426dc7126c
Domingo Hernandez Cuervo <domingo.hdez.cuervo@gmail.com>
2018-03-26 01:33:39 -0600

    Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
 declare function initPlugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -8,15 +12,49 @@ declare function initPlugins();
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  recuerdame: boolean = false;
+  email: string;
+  auth2: any;
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, public _usuario: UsuarioService) { }
 
   ngOnInit() {
-    initPlugins()
+    initPlugins();
+    this.googleInit();
+    this.email = localStorage.getItem('email') || '';
+    if (this.email.length > 0) {
+      this.recuerdame = true;
+    }
   }
 
-  ingresar() {
-    this.router.navigate(['/dashboard']);
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '686102589992-dcpdv5oj663cbv8uvol104q2am1qruoi.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin(document.getElementById('btnGoogle'));
+    });
+  }
+  attachSignin(element) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+      // let profile = googleUser.getBasicProfile();
+      let token = googleUser.getAuthResponse().id_token;
+      console.log(token);
+      this._usuario.loginGoogle(token)
+        .subscribe(resp => window.location.href = '#/dashboard');
+    });
+  }
+
+  ingresar(forma: NgForm) {
+    if (forma.invalid) {
+      return;
+    }
+    let usuario = new Usuario(null, forma.value.email, forma.value.password);
+    this._usuario.login(usuario, forma.value.recuerdame)
+      .subscribe(res => this.router.navigate(['/dashboard']));
+    // this.router.navigate(['/dashboard']);
   }
 
 }
